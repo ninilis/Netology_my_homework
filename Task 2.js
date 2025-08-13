a95fdf2566a33357a4d0fa8e0f6d76cf27cb2cd7
ninilis <latifullina1997@mail.ru>
2025-08-13 10:45:29 +0300

    Delete Task 2.js

--- Task 2.js
+++
@@ -1,9 +0,0 @@
-let user = "netologist";
-let orderOwner = "leo_tolstoy";
-
-if (user === orderOwner || user === "admin")  {
-    console.log("Редактирование разрешено");
-}
-    else {
-        console.log("Заказ нельзя редактировать")
-}
